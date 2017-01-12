c44e78e1bda8713bf518688fa656ad24327efaf9
simran bhatia <simran18@uw.edu>
2017-01-12 13:39:59 -0800

    Complete exercise 4

---
+++ exercise 4/exercise.R
@@ -0,0 +1,27 @@
+# Create a variable `lyric` that contains the text "I like to eat apples and bananas"
+
+
+# Use the `substr()` function to extract the 1st through 13th letters from the `lyric`
+# Use `?substr` to see more about this function
+# Store the result in a variable called `intro`
+
+
+# Use the `substr()` function to extract the 15th through the last letter of `lyric`
+# Hint: use `nchar()` to determine how many letters there are!
+# Store the result in a variable called `fruits`
+
+
+# Use the `gsub()` function to substitute all the "a"s in `fruits` with "ee".
+# Hint: see http://www.endmemo.com/program/R/sub.php for a simpmle example (or use `?gsub`)
+# Store the result in a variable called `fruits.e`
+
+
+# Use the `gsub()` function to substitute all the "a"s in `fruits` with "o".
+# Store the result in a variable called `fruits.o`
+
+
+# Create a new variable `lyric.e` that is the `intro` combined with the new `fruits.e` ending
+# Print out this variable
+
+
+# Print out the `intro` combined with the new `fruits.o` ending
